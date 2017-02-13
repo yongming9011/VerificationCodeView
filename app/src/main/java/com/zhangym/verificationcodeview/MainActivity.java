1541a644c207c85fb6ec6a6190bcccb38188243d
zhangym <yongming9011@163.com>
2017-02-13 11:03:22 +0800

    update

--- app/src/main/java/com/zhangym/verificationcodeview/MainActivity.java
+++ app/src/main/java/com/zhangym/verificationcodeview/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         initViews();
